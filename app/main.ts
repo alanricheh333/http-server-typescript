5c5cd49b082d55a16b06a0ac8e6531c9d22b0de8
Alan Alrechah <alan.richeh@gmail.com>
2024-07-07 12:18:32 +0200

    add 200 response

--- app/main.ts
+++ app/main.ts
@@ -5,7 +5,9 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
   socket.on("close", () => {
+    
     socket.end();
   });
 });
