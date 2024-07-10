6e8c4dd9221ffebd420f495dd7346ac15e15805f
Alan Alrechah <alan.richeh@gmail.com>
2024-07-10 19:17:30 +0200

    handle request with different paths

--- app/main.ts
+++ app/main.ts
@@ -5,11 +5,16 @@ console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
-  socket.write(Buffer.from("HTTP/1.1 200 OK\r\n\r\n"));
-  socket.on("close", () => {
-    
-    socket.end();
-  });
+    socket.on("data", (data) => {
+        const request = data.toString();
+        const path = request.split(" ")[1];
+        if (path === "/") {
+            socket.write('HTTP/1.1 200 OK\r\n\r\n');
+        } else {
+            socket.write('HTTP/1.1 404 Not Found\r\n\r\n');
+        }
+        socket.end();
+    })
 });
 
 server.listen(4221, "localhost");
