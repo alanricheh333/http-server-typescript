5000993e45ef985385d5fa9db225c3948c55b89c
Alan Alrechah <alan.richeh@gmail.com>
2024-07-26 00:49:16 +0200

    codecrafters submit [skip ci]

--- app/helper.ts
+++ app/helper.ts
@@ -1,4 +1,6 @@
 
+import fs from 'node:fs';
+
 export function generateResponseFromRequest(request: string, path: string, param: string): string {
     switch (param) {
         case '':
@@ -9,6 +11,22 @@ export function generateResponseFromRequest(request: string, path: string, param
         case 'user-agent':
             const userAgent = request.split('User-Agent: ')[1].split('\r\n')[0]
             return `HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: ${userAgent.length}\r\n\r\n${userAgent}`;
+        case 'files':
+            const fileName = path.split('/')[2]
+            const args = process.argv.slice(2);
+            const [___, absPath] = args;
+            const filePath = absPath + "/" + fileName;
+
+            try {
+                const content = fs.readFileSync(filePath);
+                console.log('File exists');
+                return `HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: ${content.length}\r\n\r\n${content}`;
+            } catch (error) {
+                console.log('File does not exist');
+                return 'HTTP/1.1 404 Not Found\r\n\r\n';
+            }
+        
+
         default:
             return 'HTTP/1.1 404 Not Found\r\n\r\n';
     }
